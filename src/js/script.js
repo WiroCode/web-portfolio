041377da13e225aad571db3fc87f072cb727dc51
wirocode <ghandoz.ngalam@gmail.com>
2025-07-21 08:36:20 +0700

    update

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,32 @@
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+// swiper
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+
 // hamburger
 
 const hamburger = document.querySelector("#hamburger");
